6f1b8545071458903d8da11b326ce444413fd5d3
MrPigHead <2519700452@qq.com>
2021-02-10 13:34:36 +0800

    20200210 MrPigHead Initial commit Hello world Method reference (package-info)

---
+++ src/main/java/mrpighead/teach/HelloWorld.java
@@ -0,0 +1,21 @@
+package mrpighead.teach;
+
+import java.util.Scanner;
+
+/**
+ * HelloWorld.java
+ * 第一个Java程序
+ */
+// 类名必须和文件名一样
+public class HelloWorld {
+
+    /**
+     * 打印出"Hello world!"字样, 换行
+     * @param args 运行时传参
+     */
+    // Java程序入口: public static void main(String[] args)
+    public static void main(String[] args) {
+        // 在控制台打印出"Hello world!"并换行
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/mrpighead/teach/MethodReference/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 2021-02-10
+ * 今天看到MC模组例程里面有个类似于C++中namespace引用(this::setup)的例子, 但Java没有
+ * namespace的概念, 就搜了一下, 是方法引用
+ *
+ * 4种情况: 静态、对象、超类、构造器
+ */
+package mrpighead.teach.MethodReference;
